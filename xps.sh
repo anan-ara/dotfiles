5bb592e4223323528978564d78a64138b9271922
anan-ara <43068235+anan-ara@users.noreply.github.com>
2021-02-08 01:01:51 +0700

    changed files are system update

--- xps.sh
+++ xps.sh
@@ -3,16 +3,13 @@
 # Start at 50% brightness
 xbacklight -set 50
 
-#HiDPI
-xrandr --dpi 120
-
 # source ~/.config/headmicfix.sh
 
 # Right click with 2 fingers instead
-xinput set-prop 'DLL075B:01 06CB:76AF Touchpad' 329 0 1
+xinput set-prop 'DLL075B:01 06CB:76AF Touchpad' 337 0 1
 # Increase trackpad sensitivity
 # xinput set-prop 'DLL075B:01 06CB:76AF Touchpad' 309 0.2
 
 # Enable tap to click (but not tap to drag)
-xinput set-prop 'DLL075B:01 06CB:76AF Touchpad' 318 1
-xinput set-prop 'DLL075B:01 06CB:76AF Touchpad' 320 0
+xinput set-prop 'DLL075B:01 06CB:76AF Touchpad' 326 1
+xinput set-prop 'DLL075B:01 06CB:76AF Touchpad' 328 0
